2fbf728168ac31cab4185f08cc3972cc38fe10d7
Jens Langerak <j.langerak@student.tudelft.nl>
2018-03-20 21:52:14 +0100

    Add RelayCommand, ViewModelBase and some example MainWindow code.

---
+++ PostIt/ViewModels/ViewModelMainWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using PostIt.ViewModels.Commands;
+
+namespace PostIt.ViewModels
+{
+    public class ViewModelMainWindow : ViewModelBase
+    {
+        private string _testString = "Hello world!";
+
+        public string TestString
+        {
+            get => _testString;
+            set => SetProperty(ref _testString, value);
+        }
+
+        public RelayCommand UpdateTekstCommand { get; set; }
+
+        public ViewModelMainWindow()
+        {
+            UpdateTekstCommand = new RelayCommand(TestCommand);
+        }
+
+        void TestCommand(object waarde)
+        {
+            TestString = (string) waarde;
+            if (TestString == "test")
+                MessageBox.Show("Test");
+        }
+
+    }
+}
